683b8300782d96dd66b3fb1c9f7bbd94f11c1341
Sean Hogan <157655532+Shogan2@users.noreply.github.com>
2024-10-17 16:42:49 -0400

    I got lazy and just copied and pasted the entire new code
    Commented out all of the setScoring() function, and I added its functionality to the increaseScore() function so that it checks the scoring type every time a button is pressed.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-﻿// test
+// test
+
 
 let scoreA = 0;
 let scoreB = 0;
@@ -10,130 +11,143 @@ let teamBName = "Team 2";
 //let servingTeam = null; /* 'A';   null for now and assumes we don't know who is serving */
 let strScoreType = document.getElementById('scoringType').value;
 
+
 function updateTeamNames() {
-    //document.querySelector('input[name="team1"]').innerHTML    = teamAName;
-    document.getElementsByName('team1')[0].innerHTML = teamAName;
-    document.getElementsByName('team2')[0].innerHTML = teamBName;
+   //document.querySelector('input[name="team1"]').innerHTML    = teamAName;
+   document.getElementsByName('team1')[0].innerHTML = teamAName;
+   document.getElementsByName('team2')[0].innerHTML = teamBName;
 }
 // set the default team names using the global variables
 updateTeamNames();
 
+
 function setScoring() {
-    selectElement = document.querySelector('#scoringType');
-    scoringType = selectElement.value;
-    document.querySelector('.output').textContent = scoringType;
+   //selectElement = document.querySelector('#scoringType');
+   //scoringType = selectElement.value;
+   //document.querySelector('.output').textContent = scoringType;
 }
+
 
 function increaseScore(team) {
-    const scoreAElement = document.querySelector('#scoreA span');
-    const scoreBElement = document.querySelector('#scoreB span');
+   selectElement = document.querySelector('#scoringType');
+   scoringType = selectElement.value;
 
-    //add functionality for sideout and rally scoring
-    if (strScoreType == 'sideout') {
-        /* sideout scoring */
-        if (team === 'A') {
-            if (servingTeam == teamA_serve) {
-                scoreA++;
-                scoreAElement.innerText = scoreA;
-            } else {
-                servingTeam = teamA_serve;
-            }
-        } else if (team === 'B') {
-            if (servingTeam == teamB_serve) {
-                scoreB++;
-                scoreBElement.innerText = scoreB;
-            } else {
-                servingTeam = teamB_serve;
-            }
-        }
-        
-    } else if (strScoreType == 'rally') {
-        /* rally scoring */
-        if (team === 'A') {
-            scoreA++;
-            scoreAElement.innerText = scoreA;
-        } else if (team === 'B') {
-            scoreB++;
-            scoreBElement.innerText = scoreB;
-        }
-    }
-    
-    //if (team === 'A') {
-    //    scoreA++;
-    //    scoreAElement.innerText = scoreA;
-        //scoreAElement.style.border = '5px solid black';
-        //scoreBElement.style.border = 'none';
-    //    servingTeam = 'A'; /* used for sideout */
-    //} else if (team === 'B') {
-    //    scoreB++;
-    //    scoreBElement.innerText = scoreB;
-        //scoreBElement.style.border = '5px solid black';
-        //scoreAElement.style.border = 'none';
-    //    servingTeam = 'B'; /* used for sideout */
-    //}
-    //console.log(servingTeam); 
+
+   const scoreAElement = document.querySelector('#scoreA span');
+   const scoreBElement = document.querySelector('#scoreB span');
+
+
+   //add functionality for sideout and rally scoring
+   if (scoringType === 'sideout') {
+       /* sideout scoring */
+       if (team === 'A') {
+           if (servingTeam == teamA_serve) {
+               scoreA++;
+               scoreAElement.innerText = scoreA;
+           } else {
+               servingTeam = teamA_serve;
+           }
+       } else if (team === 'B') {
+           if (servingTeam == teamB_serve) {
+               scoreB++;
+               scoreBElement.innerText = scoreB;
+           } else {
+               servingTeam = teamB_serve;
+           }
+       }
+      
+   } else if (scoringType === 'rally') {
+       /* rally scoring */
+       if (team === 'A') {
+           scoreA++;
+           scoreAElement.innerText = scoreA;
+       } else if (team === 'B') {
+           scoreB++;
+           scoreBElement.innerText = scoreB;
+       }
+   }
+  
+   //if (team === 'A') {
+   //    scoreA++;
+   //    scoreAElement.innerText = scoreA;
+       //scoreAElement.style.border = '5px solid black';
+       //scoreBElement.style.border = 'none';
+   //    servingTeam = 'A'; /* used for sideout */
+   //} else if (team === 'B') {
+   //    scoreB++;
+   //    scoreBElement.innerText = scoreB;
+       //scoreBElement.style.border = '5px solid black';
+       //scoreAElement.style.border = 'none';
+   //    servingTeam = 'B'; /* used for sideout */
+   //}
+   //console.log(servingTeam);
+
 
 }
+
 
 function toggleMenu() {
-    const menuOverlay = document.getElementById('menuOverlay');
-    const scoreButtons = document.querySelectorAll('.score-button');
- 
-    if (menuOverlay.style.display === 'flex') {
-        menuOverlay.style.display = 'none';
-        scoreButtons.forEach(button => button.disabled = false);
-    } else {
-        menuOverlay.style.display = 'flex';
-        scoreButtons.forEach(button => button.disabled = true);
-        document.getElementById('adjustScoreA').innerText = scoreA;
-        document.getElementById('adjustScoreB').innerText = scoreB;
-    }
-    // this section is needed to make the underline appear after the menu loses focus
-    //console.log("servingTeam is " + servingTeam);
-    if (servingTeam == "A") {
-        document.getElementById("scoreA").focus();
-        //console.log("should be A - " + servingTeam);
-    } else if (servingTeam == "B") {
-        document.getElementById("scoreB").focus();
-        //console.log("should be B - " + servingTeam);
-    }
+   const menuOverlay = document.getElementById('menuOverlay');
+   const scoreButtons = document.querySelectorAll('.score-button');
+   if (menuOverlay.style.display === 'flex') {
+       menuOverlay.style.display = 'none';
+       scoreButtons.forEach(button => button.disabled = false);
+   } else {
+       menuOverlay.style.display = 'flex';
+       scoreButtons.forEach(button => button.disabled = true);
+       document.getElementById('adjustScoreA').innerText = scoreA;
+       document.getElementById('adjustScoreB').innerText = scoreB;
+   }
+   // this section is needed to make the underline appear after the menu loses focus
+   //console.log("servingTeam is " + servingTeam);
+   if (servingTeam == "A") {
+       document.getElementById("scoreA").focus();
+       //console.log("should be A - " + servingTeam);
+   } else if (servingTeam == "B") {
+       document.getElementById("scoreB").focus();
+       //console.log("should be B - " + servingTeam);
+   }
 }
+
 
 function resetScores() {
-    scoreA = 0;
-    scoreB = 0;
-    document.querySelector('#scoreA span').innerText = scoreA;
-    document.querySelector('#scoreB span').innerText = scoreB;
-    document.getElementById('adjustScoreA').innerText = scoreA;
-    document.getElementById('adjustScoreB').innerText = scoreB;
-    servingTeam = null;
+   scoreA = 0;
+   scoreB = 0;
+   document.querySelector('#scoreA span').innerText = scoreA;
+   document.querySelector('#scoreB span').innerText = scoreB;
+   document.getElementById('adjustScoreA').innerText = scoreA;
+   document.getElementById('adjustScoreB').innerText = scoreB;
+   servingTeam = null;
 }
+
 
 function adjustScore(team, amount) {
-    if (team === 'A') {
-        scoreA = Math.max(0, scoreA + amount);
-        document.querySelector('#scoreA span').innerText = scoreA;
-        document.getElementById('adjustScoreA').innerText = scoreA;
-    } 
-    if (team === 'B') {
-        scoreB = Math.max(0, scoreB + amount);
-        document.querySelector('#scoreB span').innerText = scoreB;
-        document.getElementById('adjustScoreB').innerText = scoreB;
-    }
+   if (team === 'A') {
+       scoreA = Math.max(0, scoreA + amount);
+       document.querySelector('#scoreA span').innerText = scoreA;
+       document.getElementById('adjustScoreA').innerText = scoreA;
+   }
+   if (team === 'B') {
+       scoreB = Math.max(0, scoreB + amount);
+       document.querySelector('#scoreB span').innerText = scoreB;
+       document.getElementById('adjustScoreB').innerText = scoreB;
+   }
 }
 
+
 function switchDivs() {
-    let container = document.getElementsByClassName('container')[0];
-    container.appendChild(container.children[0]);
-    //console.log('switchted!');
-    
-    // this section is needed to make the underline appear on the right button after the switch
-    //console.log("servingTeam is " + servingTeam);
-    if (servingTeam == "A") {
-        document.getElementById("scoreA").focus();
-        //console.log("should be A - " + servingTeam);
-    } else if (servingTeam == "B") {
-        document.getElementById("scoreB").focus();
-        //console.log("should be B - " + servingTeam);
-    }
+   let container = document.getElementsByClassName('container')[0];
+   container.appendChild(container.children[0]);
+   //console.log('switchted!');
+  
+   // this section is needed to make the underline appear on the right button after the switch
+   //console.log("servingTeam is " + servingTeam);
+   if (servingTeam == "A") {
+       document.getElementById("scoreA").focus();
+       //console.log("should be A - " + servingTeam);
+   } else if (servingTeam == "B") {
+       document.getElementById("scoreB").focus();
+       //console.log("should be B - " + servingTeam);
+   }
 };
+
